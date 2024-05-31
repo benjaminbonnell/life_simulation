63eca3a53c712a01ccc8fe138078cfab3313bd00
Benjamin Bonnell <bonnelbl@dukes.jmu.edu>
2024-05-30 20:55:29 -0400

    canvas now fullscreened

---
+++ script.js
@@ -0,0 +1,112 @@
+canvas = document.getElementById("life")
+
+canvas.width = window.innerWidth - 20;
+canvas.height= window.innerHeight - 25;
+m = canvas.getContext("2d")
+
+canvasWidth = canvas.offsetWidth
+canvasHeight = canvas.offsetHeight
+
+console.log(canvasWidth)
+
+draw = (x, y, c, s) => {
+    m.fillStyle = c
+    m.fillRect(x, y, s, s)
+}
+
+particles = [];
+
+particle = (x, y, c) => {
+    return { "x": x, "y": y, "vx": 0, "vy": 0, "color": c }
+}
+
+random = (size) => {
+    return Math.random() * size + 50
+}
+
+create = (number, color) => {
+    group = []
+    for (let i = 0; i < number; i++) {
+        group.push(particle(random(canvasWidth), random(canvasHeight), color))
+        particles.push(group[i])
+    }
+    return group;
+}
+
+rule = (particles1, particles2, g) => {
+    for (i = 0; i < particles1.length; i++) {
+        fx = 0
+        fy = 0
+        for (j = 0; j < particles2.length; j++) {
+            a = particles1[i]
+            b = particles2[j]
+
+            dx = a.x - b.x
+            dy = a.y - b.y
+
+            d = Math.sqrt(dx * dx + dy * dy)
+
+            if (d > 0 && d < 80) {
+                F = g * 1 / d
+                fx += (F * dx)
+                fy += (F * dy)
+            }
+        }
+        a.vx = (a.vx + fx) * 0.5
+        a.vy = (a.vy + fy) * 0.5
+
+        a.x += a.vx
+        a.y += a.vy
+
+        if (a.x <= 0 || a.x >= canvasWidth) { a.vx *= -1 }
+        if (a.y <= 0 || a.y >= canvasHeight) { a.vy *= -1 }
+    }
+}
+
+colors = []
+
+colors.push(yellow = create(300, "yellow"))
+colors.push(red = create(300, "red"))
+colors.push(blue = create(300, "blue"))
+colors.push(green = create(300, "green"))
+
+
+
+axioms = [];
+
+
+for (i = 0; i < colors.length; i++) {
+    for (j = 0; j < colors.length; j++) {
+        const axiom = {
+            color1: colors[i],
+            color2: colors[j],
+            force: (Math.random() - 0.5) * 3,
+        };
+
+        //console.log('made rulepair:', axiom.color1[0].color, axiom.color2[0].color, axiom.force)
+
+        axioms.push(axiom);
+    }
+
+}
+
+
+
+update = () => {
+
+    for (let i = 0; i < axioms.length; i++) {
+        rule(axioms[i].color1, axioms[i].color2, axioms[i].force)
+    }
+
+    m.clearRect(0, 0, canvasHeight, canvasWidth) // clears the canvas?
+    //draw(0, 0, "black", canvasHeight)  // draws a black background
+    m.fillStyle = "black"
+    m.fillRect(0, 0, canvasWidth, canvasHeight)
+
+    for (i = 0; i < particles.length; i++) {
+        draw(particles[i].x, particles[i].y, particles[i].color, 5)
+    }
+    requestAnimationFrame(update)
+}
+
+update();
